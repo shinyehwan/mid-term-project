f62875858317c27187c7316940d2e26e09dbb761
shinyehwan <jghisjl007@naver.com>
2022-05-05 22:03:31 +0900

    chrome에서는 가능, safari 에서는 불가능하다. 일단 소개하기 전 링크 및 페이지 이동 확인

--- introduction.js
+++ introduction.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var intro = express();
 
-intro.get('/', function(req, res){
+intro.get('/index.html', function(req,res){
     res.sendfile('index.html');
+})
+intro.get('/1.html', function(req,res){
+    res.sendfile('1.html');
+})
+intro.get('/2.html', function(req,res){
+    res.sendfile('2.html');
+})
+intro.get('/3.html', function(req,res){
+    res.sendfile('3.html');
 })
 
 intro.listen(3000,function(){
